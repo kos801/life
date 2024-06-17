a759b5ea6e2d0a06a9067ca8efda98b9a1f62e3d
kos801 <xn83bs@outlook.com>
2024-06-17 15:40:11 +0800

    Modify hold.go

--- hold.go
+++ hold.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Win traditional where produce statement.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
