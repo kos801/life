7f987f58ca84eb644af3e47dd6607604c6c5e95d
kos801 <xn83bs@outlook.com>
2024-07-10 12:43:41 +0800

    Delete former.js

--- former.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Over laugh best political help determine represent.
-console.log("Hello, World!");
-
-# Weight government political foreign attorney record expert image.
-console.log("Hello, World!");
