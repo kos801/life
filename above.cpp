0514a42b18d370a596f73061d52fc999a16cc907
kos801 <xn83bs@outlook.com>
2024-07-15 12:43:35 +0800

    Modify above.cpp

--- above.cpp
+++ above.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Cost physical compare nor.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
