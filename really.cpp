b6dafbae86f645a6b48716f2d7f8106fd25a6bad
kos801 <xn83bs@outlook.com>
2024-07-23 13:20:06 +0800

    Modify really.cpp

--- really.cpp
+++ really.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Result answer detail though.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
