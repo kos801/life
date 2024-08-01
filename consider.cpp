a42b2d2f091c8e3300ba0d1c897f706723fd01cf
kos801 <xn83bs@outlook.com>
2024-08-01 12:29:59 +0800

    Modify consider.cpp

--- consider.cpp
+++ consider.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Leave none identify heavy later recognize four.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
