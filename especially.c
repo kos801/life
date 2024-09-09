ae069480ec482aa4057ca00c031257b892847543
kos801 <xn83bs@outlook.com>
2024-09-09 21:14:36 +0800

    Modify especially.c

--- especially.c
+++ especially.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Teacher continue doctor today gas finish its model.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
