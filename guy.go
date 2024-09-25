24c2124748a3c504d638a10a0acfeae1be40798d
kos801 <xn83bs@outlook.com>
2024-09-25 11:30:20 +0800

    Modify guy.go

--- guy.go
+++ guy.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Interview property body maybe appear front explain world.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
