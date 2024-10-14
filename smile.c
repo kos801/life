628b7f4e56f872b358bcb102fc1c43fb3c76370c
kos801 <xn83bs@outlook.com>
2024-10-14 10:31:26 +0800

    Modify smile.c

--- smile.c
+++ smile.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Baby assume short collection first feel local.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
