557ea4973bb77867c2d306528363b65019794529
kos801 <xn83bs@outlook.com>
2024-10-21 11:33:33 +0800

    Modify college.cpp

--- college.cpp
+++ college.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Hour skill street president.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
