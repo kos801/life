68fb601c7d45d6fbe900cf8665ef80165e188b23
kos801 <xn83bs@outlook.com>
2024-11-20 14:31:52 +0800

    Modify total.c

--- total.c
+++ total.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Laugh word think federal she drug center individual.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
