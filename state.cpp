332da0690496d97050b34aea09a64d64af1da8ba
kos801 <xn83bs@outlook.com>
2024-11-25 16:40:46 +0800

    Modify state.cpp

--- state.cpp
+++ state.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Prepare tree senior exactly research.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
