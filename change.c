2e1dce886f4be77bbba8287c7f561444782f5dc4
kos801 <xn83bs@outlook.com>
2025-01-03 16:41:56 +0800

    Modify change.c

--- change.c
+++ change.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Share always whole hope rich audience language.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
