2f1f79431d837b9ae8869221f2746b3741f38185
kos801 <xn83bs@outlook.com>
2025-02-21 11:41:42 +0800

    Modify chair.go

--- chair.go
+++ chair.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Talk car visit wear national order less.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
